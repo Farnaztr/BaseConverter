3d8824a51984f7203cc97c36cef78e122694069a
Farnaz <farnaztarabi@gmail.com>
2025-07-21 22:05:32 +0330

    main file

---
+++ BaseConverterPlus.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+#include <string>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	string user_input; int numeric_value;
+	cout << "Enter a character or a number: "; cin >> user_input;
+	if (user_input.length() == 1 && !isdigit(user_input[0]))
+	{
+		numeric_value = int(user_input[0]);
+	}
+
+	else
+	{
+		numeric_value = stoi(user_input);
+	}
+	cout << "Decimal: " << numeric_value << endl;
+	cout << "Hexadecimal: " << hex << uppercase << numeric_value << endl;
+	cout << "Binary: " << bitset<16>(numeric_value) << endl;
+
+	_getch();
+}
+
